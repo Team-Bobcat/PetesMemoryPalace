ab59aa0b4a4255c650d335c6adf879ecb974723f
programminPete <peter.fasula@gmail.com>
2017-12-09 15:54:02 -0800

    interfacing with database - push to db

--- client/components/NodeImageHandling/ImageView.jsx
+++ client/components/NodeImageHandling/ImageView.jsx
@@ -22,33 +22,41 @@ class ImageView extends Component {
       locXHover: 0,
       locYHover: 0,
       name: 'George Washington',
-      description: 'washing a tin cup'
+      description: 'washing a tin cup',
+      location: '',
+      srcImg: ''
     }
   this.handleOptionChange = this.handleOptionChange.bind(this);
   this.handleInputChange = this.handleInputChange.bind(this);
   // this.onMouseMove = this.onMouseMove.bind(this);
   this.onMouseClick = this.onMouseClick.bind(this);
   this.addNode = this.addNode.bind(this); 
-
+  this.getPalace = this.getPalace.bind(this);  
+  }
+  componentWillMount(){
+    var currentLocation = this.props.location.pathname
+    var routeArr = currentLocation.split("/")
+    this.getPalace(routeArr[2]);
   }
   componentDidMount(){
-    console.log('component mounted: Image view');
-    console.log('NodeListAPI.nodeList: ', NodeListAPI.nodeList)
-    console.log('NodeListAPI.nodeList.length: ', NodeListAPI.nodeList.length)
+
     this.setState({
       nodes: NodeListAPI.nodeList,
       id: NodeListAPI.nodeList.length});
     }
+  
 
-    fetchPalaces(){
-    fetch('/getPalaces',{
+  getPalace(location){
+    // event.preventDefault();
+    const name = location;
+    fetch(`/getPalace?palace=${name}`, {
       method: 'GET',
       credentials: 'include',
-    }).then((response) => response.json()).then(data => {
-      console.log(data);
-      this.setState({palaces: data});
-    })
-  }
+    }).then( (res) => res.json())
+      .then( (data) => {
+        this.setState({srcImg: data[0].img})
+  })
+}
 
   // will have to do differently with real API call - .all will need to be written here.
   // getAllNodes(NodeListAPI){
@@ -64,7 +72,7 @@ class ImageView extends Component {
     const target = e.target;
     const value = target.value;
     let name = target.name;
-    console.log('name, value: ', name + ' ' + value)
+    // console.log('name, value: ', name + ' ' + value)
     this.setState({
       [name]: value
     })
@@ -80,11 +88,13 @@ class ImageView extends Component {
   // handles adding and viewing node info
   onMouseClick(e) {
     if(this.state.selectedOption === 'display'){
-      console.log('DISPLAY CURRENT NODE functionality here')
+      // console.log('DISPLAY CURRENT NODE functionality here')
       // console.log(e.screenX, e.screenY)
+      // console.log(this.state.nodes)
       this.setState({
         locXHover: e.screenX,
         locYHover: e.screenY
+        
       })
     } else { // this.state.selectedOption === 'add' , add new node
       // let createNewNode = new Node; 
@@ -113,14 +123,12 @@ class ImageView extends Component {
   addNode(e){   // review how you're doing this -- should probably be doing a copy here
     e.preventDefault();
     e.stopPropagation();
-    console.log('submit')
     let tempNodes = this.state.nodes.slice();
     let len = tempNodes.length;
     let counter = this.state.id;
-    console.log('counter: in addNode: ', counter)
     // assemble data for todo
     const node = {
-      id: counter,
+      // id: counter,
       name: this.state.name,
       description: this.state.description,
       image: 'placeholder',
@@ -130,19 +138,20 @@ class ImageView extends Component {
     //update data
     tempNodes.push(node)
     NodeListAPI.nodeList.push(node)
-
+    console.log(node)
+    // console.log('JSON stringify: ', JSON.stringify(node))
     //make push to database
-    // fetch('http://localhost:8080/api', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(nodeAdd)
-    // }).then((res) => {
-    //   console.log("this is res", res)
-    // }).catch((err) => {
-    //   console.log(err)
-    // });
+    fetch('http://localhost:3000/storeNode', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(node)
+    }).then((res) => {
+      console.log("this is res", res)
+    }).catch((err) => {
+      console.log(err)
+    });
     counter += 1;
     //update state
     this.setState({
@@ -155,13 +164,12 @@ class ImageView extends Component {
 
 
   render() {
-    NodeListAPI.nodeList.map( (node) => {
-      console.log('name:', node.name)
-      console.log('node id:', node.id)
-      })
+    // NodeListAPI.nodeList.map( (node) => {
+    //   console.log('name:', node.name)
+    //   console.log('node id:', node.id)
+    //   })
     var renderThis;
     if(this.state.selectedOption === 'display'){
-      console.log('here')
       renderThis = <NodeDescription 
                       name={this.state.name}
                       description={this.state.description} />
@@ -194,6 +202,7 @@ class ImageView extends Component {
                 locY={this.state.locY}
                 locXHover={this.state.locXHover}
                 locYHover={this.state.locYHover}
+                srcImg={this.state.srcImg}
                 onMouseMove={this.onMouseMove}
                 onMouseClick={this.onMouseClick}
                 selectedOption={this.state.selectedOption} />
